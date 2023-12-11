05a8badab739214273862e6f5757ec7eb82ff620
Nemanja Mijailovic <nemanja.mijailovic2912@gmail.com>
2023-12-11 05:55:29 +0100

    feat: routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-// import {
-//   Route,
-//   RouterProvider,
-//   createBrowserRouter,
-//   createHashRouter,
-//   createRoutesFromElements,
-// } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createHashRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import About from "./screens/about";
 import Home from "./screens/home";
 import "./styles/general.css";
@@ -12,80 +12,44 @@ import FrontLayout from "./layout/frontLayout";
 import Faq from "./screens/faq";
 import Blog from "./screens/blog";
 
-// const router = createHashRouter(
-//   createRoutesFromElements(
-//     <Route path="/">
-//       <Route
-//         index
-//         element={
-//           <FrontLayout>
-//             <Home />
-//           </FrontLayout>
-//         }
-//       />
-//       <Route
-//         path="about"
-//         element={
-//           <FrontLayout>
-//             <About />
-//           </FrontLayout>
-//         }
-//       />
-//       <Route
-//         path="faq"
-//         element={
-//           <FrontLayout>
-//             <Faq />
-//           </FrontLayout>
-//         }
-//       />
-//       <Route
-//         path="blog"
-//         element={
-//           <FrontLayout>
-//             <Blog />
-//           </FrontLayout>
-//         }
-//       />
-//     </Route>
-//   )
-// );
-
-// function App() {
-//   return (
-//     <>
-//       <RouterProvider router={router} />
-//     </>
-//   );
-// }
-
-// export default App;
-
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/mandl_frontend/",
-    children: [
-      {
-        path: "/mandl_frontend/",
-        element: (
+const router = createHashRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route
+        index
+        element={
           <FrontLayout>
             <Home />
           </FrontLayout>
-        ),
-      },
-      {
-        path: "/mandl_frontend/about",
-        element: (
+        }
+      />
+      <Route
+        path="about"
+        element={
           <FrontLayout>
             <About />
           </FrontLayout>
-        ),
-      },
-    ],
-  },
-]);
+        }
+      />
+      <Route
+        path="faq"
+        element={
+          <FrontLayout>
+            <Faq />
+          </FrontLayout>
+        }
+      />
+      <Route
+        path="blog"
+        element={
+          <FrontLayout>
+            <Blog />
+          </FrontLayout>
+        }
+      />
+    </Route>
+  )
+);
 
 function App() {
   return (
