25316b981ada312cdf8886a57a0b31ab876612eb
Nemanja Mijailovic <nemanja.mijailovic2912@gmail.com>
2023-12-11 05:57:34 +0100

    feat: routing

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(() => {
   const config = {
     plugins: [react()],
-    base: "/",
+    base: "/mandl_frontend/",
   };
 
   return config;
