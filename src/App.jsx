97443d85271d9796a3d794f28370f0ac7412f37f
Nemanja Mijailovic <nemanja.mijailovic2912@gmail.com>
2023-12-11 05:38:15 +0100

    change to hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import {
   Route,
-  Routes, // Change from createRoutesFromElements to Routes
-  HashRouter as Router, // Use BrowserRouter instead of createBrowserRouter
+  RouterProvider,
+  createBrowserRouter,
+  createHashRouter,
+  createRoutesFromElements,
 } from "react-router-dom";
 import About from "./screens/about";
 import Home from "./screens/home";
@@ -10,45 +12,52 @@ import FrontLayout from "./layout/frontLayout";
 import Faq from "./screens/faq";
 import Blog from "./screens/blog";
 
+const router = createHashRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route
+        index
+        element={
+          <FrontLayout>
+            <Home />
+          </FrontLayout>
+        }
+      />
+      <Route
+        path="about"
+        element={
+          <FrontLayout>
+            <About />
+          </FrontLayout>
+        }
+      />
+      <Route
+        path="faq"
+        element={
+          <FrontLayout>
+            <Faq />
+          </FrontLayout>
+        }
+      />
+      <Route
+        path="blog"
+        element={
+          <FrontLayout>
+            <Blog />
+          </FrontLayout>
+        }
+      />
+    </Route>
+  )
+);
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <FrontLayout>
-              <Home />
-            </FrontLayout>
-          }
-        />
-        <Route
-          path="about"
-          element={
-            <FrontLayout>
-              <About />
-            </FrontLayout>
-          }
-        />
-        <Route
-          path="faq"
-          element={
-            <FrontLayout>
-              <Faq />
-            </FrontLayout>
-          }
-        />
-        <Route
-          path="blog"
-          element={
-            <FrontLayout>
-              <Blog />
-            </FrontLayout>
-          }
-        />
-      </Routes>
-    </Router>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
+  return <></>;
 }
 
 export default App;
